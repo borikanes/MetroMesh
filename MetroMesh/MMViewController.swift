9cd4dd724f03d074e94ada7ede5697918f0efcd4
borikanes <ooludemi@terpmail.umd.edu>
2016-01-19 02:09:08 -0500

    committing changes

---
+++ MetroMesh/MMViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MetroMesh
+//
+//  Created by Oluwabori Oludemi on 12/24/15.
+//  Copyright © 2015 Oluwabori Oludemi. All rights reserved.
+//
+
+import UIKit
+
+class MMViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+/*
+    if(![CLLocationManager locationServicesEnabled])
+    {
+    //You need to enable Location Services
+    }
+    if(![CLLocationManager isMonitoringAvailableForClass:[CLRegion class]])
+    {
+    //Region monitoring is not available for this Class;
+    }
+    if([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied ||
+    [CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted  )
+    {
+    //You need to authorize Location Services for the APP
+    }
+    */
+}
+
