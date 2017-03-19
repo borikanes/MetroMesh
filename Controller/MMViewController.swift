447abc0de2ecc13736e241b7a559dc0389f77541
Bori Oludemi <borikanes@Boris-MacBook-Pro.local>
2017-03-18 22:40:46 -0400

    addded fonts and now able to get user location

--- Controller/MMViewController.swift
+++ Controller/MMViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import Foundation
+import CoreLocation
 import UIKit
 
 class MMViewController: UIViewController {
+    
+    var locationManager:CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func currentLocationClicked(_ sender: UIButton) {
+        getCurrentLocation()
+    }
+    
+    func getCurrentLocation()  {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            debugPrint("Allowed to get current location")
+            locationManager.startUpdatingLocation()
+        }
+        else{
+            debugPrint("Whoops, you didn't enable location")
+        }
+    }
+    
+}
+
+extension MMViewController: CLLocationManagerDelegate{
+    /*
+     *  locationManager:didUpdateLocations:
+     *
+     *  Discussion:
+     *    Invoked when new locations are available.  Required for delivery of
+     *    deferred locations.  If implemented, updates will
+     *    not be delivered to locationManager:didUpdateToLocation:fromLocation:
+     *
+     *    locations is an array of CLLocation objects in chronological order.
+     */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            debugPrint("Error fetching location from [CLLocation]")
+            return
+        }
+        print("Lat: \(location.coordinate.latitude) --- Lon: \(location.coordinate.longitude)")
+    }
 }
